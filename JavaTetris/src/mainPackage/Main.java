1c7af81ac6dae1189ac585472e2a72fea8664907
mrgoodrich <mryangoodrich@icloud.com>
2015-02-03 15:32:57 -0800

    Added new classes

--- JavaTetris/src/mainPackage/Main.java
+++ JavaTetris/src/mainPackage/Main.java
@@ -1,8 +1,15 @@
 package mainPackage;
 
 public class Main {
+	//Init variables for main class
 	static int start = 1;
 	static int currentFrame = 0;
+	
+	//Init objects of other classes
+	Board board = new Board();
+	Shape shape  = new Shape();
+	Scoreboard scoreboard = new Scoreboard();
+	
 	public static void main(String[] args){
 		while (start==1){
 			int frame = currentFrame/100000;
